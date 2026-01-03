c319e223e5482468a27ce2fd6e7b169110af75c2
Nathaniel <nathanielabebe32@gmail.com>
2026-01-04 01:19:20 +0300

    error with migration deletion of init file

--- laundry_api/settings.py
+++ laundry_api/settings.py
@@ -141,9 +141,9 @@ WSGI_APPLICATION = 'laundry_api.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME", ""),
-        "USER": os.environ.get("DB_USER", ""),
-        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "NAME": os.environ.get("DB_NAME", "laundry_db"),
+        "USER": os.environ.get("DB_USER", "root"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "root"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
